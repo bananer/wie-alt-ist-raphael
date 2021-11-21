6e104c8cdcc5069ac40dcaeb62b46a706fa3c2b1
Philip Nagler-Frank <philip@nagler.world>
2021-11-21 12:11:54 +0100

    fix types

--- src/durations.ts
+++ src/durations.ts
@@ -1,7 +1,6 @@
 import { Temporal } from "@js-temporal/polyfill"
 
 const timeZone = 'Europe/Berlin'
-const locale = 'de-DE'
 const birth = Temporal.ZonedDateTime.from({
     timeZone,
     year: 2021,
@@ -15,7 +14,8 @@ const now = Temporal.Now.zonedDateTimeISO(timeZone)
 
 const durations: string[] = []
 
-const fieldNames: { [ k in keyof Temporal.Duration]: [string, string] } = {
+type Unit = ('years'|'months'|'weeks'|'days'|'hours'|'minutes'|'seconds')
+const fieldNames: { [k in Unit]: [string, string] } = {
     years: ['Jahr', 'Jahre'],
     months: ['Monat', 'Monate'],
     weeks: ['Woche', 'Wochen'],
@@ -25,10 +25,10 @@ const fieldNames: { [ k in keyof Temporal.Duration]: [string, string] } = {
     seconds: ['Sekunde', 'Sekunden'],
 }
 
-function fmt(duration: Temporal.Duration, fields: (keyof Temporal.Duration)[]) {
+function fmt(duration: Temporal.Duration, fields: Unit[]) {
     return fields.map(f => {
         const val = duration[f];
-        return `${val} ${fieldNames[f][val == 1 ? 0 : 1]}`
+        return `${val} ${fieldNames[f]![val == 1 ? 0 : 1]}`
     }).join(", ")
 }
 
